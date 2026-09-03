11a4db9b37346140dd398ef92e73e262f4993642
AryaTiwari <87369808+AryaTiwari@users.noreply.github.com>
2026-09-03 19:45:56 +0530

    chore(mark3): normalize voice orchestrator import

--- mark3-development/core/voice-orchestrator.js
+++ mark3-development/core/voice-orchestrator.js
@@ -1,4 +1,4 @@
-const path = require('./config') && require('path');
+const path = require('path');
 const config = require('./config');
 const { readJson, writeJsonAtomic } = require('./persistence');
 const { emit } = require('./events');
